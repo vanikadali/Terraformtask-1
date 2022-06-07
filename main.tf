eba8945bbe7e2af5ce3cb6a7d5b84df8151369c0
vanikadali <103553658+vanikadali@users.noreply.github.com>
2022-06-07 10:58:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
 
+provider "aws" { 
+
+   region = "us-east-1" 
+
+ } 
+
+ resource "aws_instance" "vm" {
+
+    ami = "ami-09d56f8956ab235b3"
+
+    instance_type = "t3.micro" 
+
+ tags = { 
+
+    Name = "my-first-tf-node"
+
+   } 
+
+}
